49c1d85f053fc2981981abe222cbb10028c116dc
aayushjoshi244 <aayush09345@gmail.com>
2026-05-20 20:09:44 +0530

    added the deletion opreation of linked list

--- linkedlist/LL.java
+++ linkedlist/LL.java
@@ -1,61 +1,94 @@
 package linkedlist;
 
-class LL{
+class LL {
     Node head;
 
-    class Node{
+    class Node {
         String data;
         Node next;
-        Node(String data){
+
+        Node(String data) {
             this.data = data;
             this.next = null;
         }
+    }
 
-        public void addFirst(String data){
-            Node newNode = new Node(data);
-            if (head == null) {
-                head = newNode;
-                return;
-            }
-            newNode.next = head;
+    public void addFirst(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
             head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void addLast(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node currNode = head;
+        while (currNode.next != null) {
+            currNode = currNode.next;
+        }
+        currNode.next = newNode;
+    }
+
+    public void printList() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        Node currNode = head;
+        while (currNode != null) {
+            System.out.print(currNode.data + " -> ");
+            currNode = currNode.next;
+        }
+        System.out.println("null");
+    }
+
+    public void delFirst(){
+        if (head == null) {
+            System.out.println("This is an empty list");
+            return;
+        }
+        head = head.next;
+    }
+
+    public void delLast() {
+        if (head == null) {
+            System.out.println("This is an empty list");
+            return;
         }
 
-        public void addLast(String data) {
-            Node newNode = new Node(data);
-            if (head == null) {
-                head = newNode;
-                return;
-            }
-            Node currNode = head;
-            while (currNode.next != null) {
-                currNode = currNode.next;
-            }
-            currNode.next = newNode;
+        if (head.next == null) {
+            head = null;
+            return;
         }
 
-        public void printList() {
-            if (head == null) {
-                System.out.println("List is empty");
-                return;
-            }
-            Node currNode = head;
-            while (currNode != null) {
-                System.out.print(currNode.data + "->");
-                currNode = currNode.next;
-            }
-            System.out.println("null");
+        Node secondLast = head;
+        Node lastNode = head.next;
+        while (lastNode.next != null) {
+            lastNode = lastNode.next;
+            secondLast = secondLast.next;
         }
+        secondLast.next = null;
+    }
 
-        public static void main(String[] args){
-            LL list = new LL();
-            list.addFirst("a");
-            list.addFirst("is");
-            list.printList();
-            list.addLast("list");
-            list.printList();
-            list.addFirst("this");
-            list.printList();
-        }
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.addFirst("a");
+        list.addFirst("is");
+        list.printList();
+        list.addLast("list");
+        list.printList();
+        list.addFirst("this");
+        list.printList();
+        list.delFirst();
+        list.printList();
+        list.delLast();
+        list.printList();
     }
 }
