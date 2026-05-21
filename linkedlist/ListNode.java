7a6647c670e475ee38ed94bf69014500f5be84da
aayushjoshi244 <aayush09345@gmail.com>
2026-05-21 22:26:29 +0530

    remove nth node  from the end

--- linkedlist/ListNode.java
+++ linkedlist/ListNode.java
@@ -73,4 +73,21 @@ class Solutions {
         }
         return dummy.next;
     }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next=head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+
+        for (int i=0;i<=n;i++) {
+            first = first.next;
+        }
+        while (first!=null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    } 
 }
