918863d0bb50798a9aa3f5fc5dc630679b1c6495
aayushjoshi244 <aayush09345@gmail.com>
2026-05-21 18:17:56 +0530

    merging two linked list

--- linkedlist/ListNode.java
+++ linkedlist/ListNode.java
@@ -50,4 +50,27 @@ class Solutions {
         }
         return dummy.next;
     }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while (list1!=null && list2!=null) {
+            if (list1.val <= list2.val) {
+                curr.next = new ListNode(list1.val);
+                list1 = list1.next;
+            } else {
+                curr.next = new ListNode(list2.val);
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+        if (list1!=null) {
+                curr.next = new ListNode(list1.val);
+        }
+        if (list2!=null) {
+                curr.next = new ListNode(list2.val);
+        }
+        return dummy.next;
+    }
 }
